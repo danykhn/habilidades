2392fa3d73401b98ce925f2bfacbef17fc1b6d17
danykhn <danjel2014@gmail.com>
2024-09-01 22:16:55 -0300

    test1

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CardModule,ButtonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export  default class ProfileComponent {
+
+
+export  default class ProfileComponent implements OnInit {
+
+
+ data:any
+constructor(  private route: ActivatedRoute,){
+}
+
+
+  ngOnInit(): void {
+
+    let id = ''
+    this.route.params.subscribe(params => id = params['id']);
+    console.log(id)
+
+    const DATA = [
+      {
+        id:1,
+        nombre:'Federico',
+        alias:'federico',
+        img: 'assets/img/federico2.jpg'
+      },
+      {
+        id:2,
+        nombre:'Santiago',
+        alias:'santiago',
+        img: 'assets/img/santiago2.jpg'
+      },
+      {
+        id:3,
+        nombre:'Lucas',
+        alias:'lucas',
+        img: 'assets/img/lucas2.jpg'
+      },
+    ]
+
+  const a =  DATA.filter((e)=>{
+    return e.alias == id
+    })
+
+  this.data = a
+
+  }
+
+
 
 }
